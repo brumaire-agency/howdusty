9d1a9ba76bb80ae944396c34ff6c9bd69de38a0a
Zoé Meriet <43410985+zoemeriet@users.noreply.github.com>
2023-06-09 15:34:17 +0200

    fix: add metrics logic (#18)

--- src/github/github.api.ts
+++ src/github/github.api.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { gql, GraphQLClient } from 'graphql-request';
-import {
-  ContributionsByRepository,
-  CreatedRepositoryContribution,
-  GetContributorInfoQuery,
-  User,
-} from './types';
+import { Metric, TotalContributionsMetric, UserInfoMetric } from './metrics';
+import { User } from './types';
 
 @Injectable()
 export class GithubApi {
-  constructor(private readonly config: ConfigService) {}
+  private metrics: Metric[];
+
+  constructor(private readonly config: ConfigService) {
+    this.metrics = [new UserInfoMetric(), new TotalContributionsMetric()];
+  }
 
   /**
    * Gets contributor info from github.
@@ -24,158 +24,28 @@ export class GithubApi {
       },
     });
 
-    const query = gql`
-      {
-        user(login: "${contributorUsername}") {
-          avatarUrl
-          login
-          name
-          id
-          contributionsCollection {
-            repositoryContributions(first: 100) {
-              nodes {
-                repository {
-                  name
-                  licenseInfo {
-                    key
-                  }
-                  isFork
-                  isPrivate
-                }
-              }
-            }
-            commitContributionsByRepository {
-              contributions(first: 100) {
-                totalCount
-              }
-              repository {
-                licenseInfo {
-                  key
-                }
-                isFork
-                isPrivate
-              }
-            }
-            issueContributionsByRepository {
-              contributions(first: 100) {
-                totalCount
-              }
-              repository {
-                licenseInfo {
-                  key
-                }
-                isFork
-                isPrivate
-              }
-            }
-            pullRequestContributionsByRepository {
-              contributions(first: 100) {
-                totalCount
-              }
-              repository {
-                licenseInfo {
-                  key
-                }
-                isFork
-                isPrivate
-              }
-            }
-          }
+    const query = this.metrics.reduce(
+      (accumulator, currentValue) =>
+        `${accumulator} ${currentValue.buildQuery(contributorUsername)}`,
+      '',
+    );
+
+    const result = await graphQLClient.request(
+      gql`
+        query {
+          ${query}
         }
-      }
-    `;
-    const result: GetContributorInfoQuery = await graphQLClient.request(query);
-
-    // Repositories
-    const repositoriesContributions: CreatedRepositoryContribution[] =
-      this.createdOpenSourceRepositories(
-        result.user.contributionsCollection.repositoryContributions.nodes,
-      );
-    const totalRepositoriesContributions = repositoriesContributions.length;
-
-    // Commit
-    const commitContributions: ContributionsByRepository[] =
-      this.contributionsFromOpenSourceRepositories(
-        result.user.contributionsCollection.commitContributionsByRepository,
-      );
-    const totalCommitContributions =
-      this.totalContributionsFromRepositories(commitContributions);
-
-    // Issue
-    const issueContributions: ContributionsByRepository[] =
-      this.contributionsFromOpenSourceRepositories(
-        result.user.contributionsCollection.issueContributionsByRepository,
-      );
-    const totalIssueContributions =
-      this.totalContributionsFromRepositories(issueContributions);
-
-    // Pull Request
-    const pullRequestContributions: ContributionsByRepository[] =
-      this.contributionsFromOpenSourceRepositories(
-        result.user.contributionsCollection
-          .pullRequestContributionsByRepository,
-      );
-    const totalPullRequestContributions =
-      this.totalContributionsFromRepositories(pullRequestContributions);
-
-    return {
-      id: result.user.id,
-      username: result.user.login,
-      name: result.user.name,
-      avatarUrl: result.user.avatarUrl,
-      totalContributions:
-        totalRepositoriesContributions +
-        totalCommitContributions +
-        totalIssueContributions +
-        totalPullRequestContributions,
-    };
-  }
-
-  /**
-   * Return contributions from open source repositories.
-   */
-  contributionsFromOpenSourceRepositories(
-    contributionsByRepository: ContributionsByRepository[],
-  ): ContributionsByRepository[] {
-    const openSourceLicenses = ['mit', 'apache-2.0', 'gpl-3.0', 'agpl-3.0'];
-    return contributionsByRepository.filter(
-      (contributionByRepository) =>
-        !contributionByRepository.repository.isFork &&
-        !contributionByRepository.repository.isPrivate &&
-        openSourceLicenses.includes(
-          contributionByRepository.repository.licenseInfo?.key,
-        ),
+      `,
     );
-  }
 
-  /**
-   * Return created open source repositories.
-   */
-  createdOpenSourceRepositories(
-    createdRepositories: CreatedRepositoryContribution[],
-  ): CreatedRepositoryContribution[] {
-    const openSourceLicenses = ['mit', 'apache-2.0', 'gpl-3.0', 'agpl-3.0'];
-    return createdRepositories.filter(
-      (createdRepository) =>
-        !createdRepository.repository.isFork &&
-        !createdRepository.repository.isPrivate &&
-        openSourceLicenses.includes(
-          createdRepository.repository.licenseInfo?.key,
-        ),
+    const data = this.metrics.reduce(
+      (accumulator, currentValue) => ({
+        ...accumulator,
+        ...currentValue.parseResult(result),
+      }),
+      {},
     );
-  }
 
-  /**
-   * Return total contributions.
-   */
-  totalContributionsFromRepositories(
-    contributionsByRepository: ContributionsByRepository[],
-  ) {
-    let totalContributions = 0;
-    contributionsByRepository.forEach((contributionByRepository) => {
-      totalContributions =
-        totalContributions + contributionByRepository.contributions.totalCount;
-    });
-    return totalContributions;
+    return data as User;
   }
 }
---
+++ src/github/metrics/total-contributions.metric.spec.ts
@@ -0,0 +1,11 @@
+import { TotalContributionsMetric } from './total-contributions.metric';
+import response from './query-response.example.json';
+
+describe('TotalContributions Metric', () => {
+  const metric = new TotalContributionsMetric();
+
+  it('parseResult should return a User', async () => {
+    const data = await metric.parseResult(response);
+    expect(typeof data.totalContributions).toBe('number');
+  });
+});
