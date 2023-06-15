f98a17639442de582d7810a751207847e457884d
zoemeriet <zoe@binch-solutions.com>
2023-06-15 16:33:54 +0200

    feat: add active contribution weeks metric

---
+++ src/github/metrics/active-contribution-weeks.metric.spec.ts
@@ -0,0 +1,11 @@
+import { ActiveContributionWeeksMetric } from './active-contribution-weeks.metric';
+import response from './query-response.example.json';
+
+describe('Active Contribution Weeks Metric', () => {
+  const metric = new ActiveContributionWeeksMetric();
+
+  it('parseResult should return a User', async () => {
+    const data = await metric.parseResult(response);
+    expect(typeof data.activeContributionWeeks).toBe('number');
+  });
+});
