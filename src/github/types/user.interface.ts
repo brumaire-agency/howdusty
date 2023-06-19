06602647b786a6be187eebd927fce547fae61116
Zoé Meriet <43410985+zoemeriet@users.noreply.github.com>
2023-06-19 15:20:21 +0200

    fix: github user name can be nullable (#49)

--- src/github/types/user.interface.ts
+++ src/github/types/user.interface.ts
@@ -1,7 +1,7 @@
 export interface User {
   id: string;
   username: string;
-  name: string;
+  name?: string;
   avatarUrl: string;
   totalContributions: number;
   contributedRepositoryCount: number;
