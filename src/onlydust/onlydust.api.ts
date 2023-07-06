ff5ef123d44c0bc4d6b4b2107fa0d25e40804bd8
zoemeriet <zoe@binch-solutions.com>
2023-07-06 15:11:48 +0200

    feat: update request onlydust

--- src/onlydust/onlydust.api.ts
+++ src/onlydust/onlydust.api.ts
@@ -26,7 +26,16 @@ export class OnlydustApi {
     await client.connect();
 
     const result = await client.query(
-      'SELECT id, login FROM "public"."github_users"',
+      `
+      SELECT id, login, SUM(money_granted) AS amount_of_grant
+      FROM (
+        SELECT users.id, users.login, payments.money_granted
+        FROM public.github_users AS users 
+        LEFT JOIN public.payment_stats AS payments 
+        ON users.id = payments.github_user_id
+      ) AS onlydust
+      GROUP BY id, login
+      `,
     );
 
     await client.end();
--- src/onlydust/types/onlydust-user.interface.ts
+++ src/onlydust/types/onlydust-user.interface.ts
@@ -1,4 +1,5 @@
 export interface OnlydustUser {
   id: string;
   login: string;
+  amount_of_grant?: number;
 }
