1af40d688f376d1074bfece1b073f5add1804cb8
Koudous Salifou <koudous@brumaire.io>
2023-08-11 11:39:22 +0200

    refactor: remove commented code

--- src/contributors/contributor.factory.ts
+++ src/contributors/contributor.factory.ts
@@ -45,48 +45,4 @@ export class ContributorFactory {
       ContributorFactory.generateContributorInfo(preset),
     );
   }
-
-  // public static generateMetrics(
-  //   preset: Partial<ContributorDto> = {},
-  // ): ContributorMetrics {
-  //   return Object.assign(
-  //     {
-  //       totalContributions: faker.number.int(1000),
-  //       contributedRepositoryCount: faker.number.int(10),
-  //       maintainedRepositoryCount: faker.number.int(10),
-  //       issuePullRequestRatio: faker.number.float({
-  //         min: 0.01,
-  //         max: 0.99,
-  //         precision: 0.01,
-  //       }),
-  //       activeContributionWeeks: faker.number.int(10),
-  //       collectedGrant: faker.number.int(5000),
-  //       meanGrantPerProject: faker.number.int(500),
-  //       contributedProjectCount: faker.number.int(10),
-  //       contributionCount: faker.number.int(20),
-  //     },
-  //     preset,
-  //   );
-  // }
-
-  // public static genrerateContributorMetrics(
-  //   preset: Partial<ContributorDto> = {},
-  // ) {
-  //   return Object.assign(
-  //     {
-  //       id: faker.string.uuid(),
-  //       ...this.generateMetrics(),
-  //     },
-  //     preset,
-  //   );
-  // }
-
-  // public static generateManyContributorMetrics(
-  //   count: number,
-  //   preset: Partial<ContributorDto> = {},
-  // ): any[] {
-  //   return Array.from(new Array(count)).map(() =>
-  //     ContributorFactory.genrerateContributorMetrics(preset),
-  //   );
-  // }
 }
